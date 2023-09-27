e2ebaa75bc15fb3618eeecc316e29c3d5d26af12
DESKTOP-I8320EU\USER <08271106mm@gmail.com>
2023-09-27 21:22:23 +0900

    フィールドの追加/カードの移動プログラム追加

--- Assets/Resources/Scripts/CardController.cs
+++ Assets/Resources/Scripts/CardController.cs
@@ -3,37 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CardController : MonoBehaviour
-{
-    CardView view;          
-    public CardModel model;        
+{        
+    //public CardModel model;        
     public CardMovement movement;  
 
     private void Awake()
     {
-        view = GetComponent<CardView>();
         movement = GetComponent<CardMovement>();
     }
 
     public void Init(int cardID)
     {
-        model = new CardModel(cardID);
+        //model = new CardModel(cardID);
     }
 
     public void Attack(CardController enemyCard)
     {
-        model.Attack(enemyCard);
-        view.SetActiveSelecablePanel(false);
+        //model.Attack(enemyCard);
     }
 
     public void CheckAlive()
     {
-        if (model.isAlive)
-        {
-            view.Refresh(model);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
+        // if (model.isAlive)
+        // {
+
+        // }
+        // else
+        // {
+        //     Destroy(this.gameObject);
+        // }
     }
 }

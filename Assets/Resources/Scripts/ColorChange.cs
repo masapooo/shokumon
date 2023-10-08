31ad4e7349024a9a14d0805cb5436fb84d86f703
DESKTOP-I8320EU\USER <08271106mm@gmail.com>
2023-10-08 23:08:13 +0900

    パネル位置調整,ドロー準備

---
+++ Assets/Resources/Scripts/ColorChange.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+  MeshRenderer mr;
+
+  void Start ()
+  {
+    mr = GetComponent<MeshRenderer>();
+    mr.material.color = mr.material.color - new Color32(0,0,0,255);
+  }
+}

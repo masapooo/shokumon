6e8ac0350fcd1c7a0dd146e052afe2e37b852013
DESKTOP-I8320EU\USER <08271106mm@gmail.com>
2023-10-29 21:11:54 +0900

    動画2:35 Database追加途中

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 //カードを配る
-    [SerializeField]
+    [SerializeField]CardDatabase ;
     Transform playerHandTransform,
                                playerFieldTransform,
                                enemyHandTransform,
